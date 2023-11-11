10416250dc82092379f5e3e5d6293f2b7863ff41
Corofides <J.l.lendrem@smoothlines.net>
2023-11-11 00:26:09 +0000

    Create an isometric cube; create an isometric square.

--- src/main.lua
+++ src/main.lua
@@ -9,6 +9,38 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+import "IsometricCube"
+import "IsometricSquare"
+
+local pd <const> = playdate
+local gfx <const> = playdate.graphics
+
+local pdWidth <const> = 400;
+local pdHeight <const> = 240
+
+local startPosX <const> = 200;
+local startPosY <const> = 144;
+
+IsometricCube(startPosX, startPosY);
+IsometricCube(startPosX - 32, startPosY - 32)
+
+
+for i = 0,10,1 do
+
+   local addY <const> = 16 * i;
+
+   for y = 0,10,1 do
+      --- isometricSquare = IsometricSquare(40 + 32 * y, 64 + addY)
+   end
+
+end
+
+
+
 function playdate.update()
    --- Main Game Loop
+
+   gfx.sprite.update()
+   pd.timer.updateTimers()
+
 end
